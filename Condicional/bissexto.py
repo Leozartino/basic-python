f7282d1d2df502c3fa1f5b748819400c7f9f5f50
Leozartino <leozartino@gmail.com>
2019-02-08 16:54:47 -0300

    add teste

--- Condicional/bissexto.py
+++ Condicional/bissexto.py
@@ -1,5 +1,7 @@
-ano = int(input())
+ano = int(input("\nInsert the number: "))
 if ano%4==0 and ano%100!=0 or ano%400==0:
     print("Ano Bissexto")
 else:
     print("Não é um ano Bissexto!")
+    #dsadsd
+    #slfjdslfjs
